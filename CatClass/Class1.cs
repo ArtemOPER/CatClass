5eb97e38d936abf8867c2367a1dda2ef6b0cd1e9
Артём_ПК <_@WIN-1CE93UBQJN5>
2020-05-06 10:54:13 +0300

    Добавьте файлы проекта.

---
+++ CatClass/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatClass
+{
+    public class Cat
+    {
+        public string Name { get ; set: }
+        public int Age { get ; set: }
+        protected int Price { get; set }
+        public Cat()
+        {
+            Price = 0;
+        }
+        public virtual string GetPrica()
+        {
+            return " Кошку можно получить бесплатно";
+        }
+    }
+
+
+    public class PurebredCat : Cat
+    {
+        public string NamePurebred { get; set; }
+
+        public string PassportID { get; set; }
+
+        public string[] Graft { get; set; }
+
+        public override string GetPrica()
+        {
+            return "Это пародистая кошка  - ее можно приобрести за" + base.GetPrica();
+        }
+    }
+    
+    public class thoroughbredcat : Cat
+
+
+        
---
+++ CatClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Шотландская_вислоухая cat = new Шотландская_вислоухая();
+            cat.name = "Шотландская вислоухая";
+            cat.price = 6000;
+            cat.age = 2;
+            cat.place = "Китай";
+
+            cat.GetCatInfo();
+
+            Русская_голубая cat1 = new Русская_голубая();
+            cat1.type = "Семейство кошачих";
+            cat1.name = "Русская голубая";
+            cat1.place = "Россия";
+            cat1.price = 100;
+            cat1.age = 5;
+
+
+            cat1.GetCat1Info();
+
+            Console.ReadLine();
+        }
+    }
+}
